29471cdb206dab43a8b9b9d9375f4b726b7c1d28
victor Macharia <Techvictor653@gmail.com>
2025-01-14 19:44:29 +0300

    feat: Added the call to action slice

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,6 +6,7 @@ import ButtonLink from "@/components/buttonLink";
 import { Content } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import { MdMenu, MdClose } from "react-icons/md";
 
 interface NavbarProps {
   settings: Content.SettingsDocument;
@@ -15,18 +16,24 @@ const NavBar = ({ settings }: NavbarProps) => {
   return (
     <nav aria-label="Main" className="px-4 py-4 md:px-6 md:py-6">
       <div className="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
-        <Link href="/">
-          <WordMark />
-          <span className="sr-only">Glisten.ai Home Page</span>
-        </Link>
-        <ul className="flex gap-6 items-center">
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <WordMark />
+            <span className="sr-only">Glisten.ai Home Page</span>
+          </Link>
+
+          <button>
+            {" "}
+            <MdMenu /> <span className="sr-only">Open Menu</span>
+          </button>
+        </div>
+
+        <ul className="flex items-center gap-6">
           {settings.data.navigation.map((item) => {
             if (item.cta_button) {
               return (
-                <li  key={item.label} >
-                  <ButtonLink field={item.link}>
-                    {item.label}
-                  </ButtonLink>
+                <li key={item.label}>
+                  <ButtonLink field={item.link}>{item.label}</ButtonLink>
                 </li>
               );
             }
