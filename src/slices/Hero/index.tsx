f8f3e8781cae021a8ab7dd886f0617b0c9f6a79a
victor Macharia <Techvictor653@gmail.com>
2025-01-16 17:06:56 +0300

    update: Added animations to the hero

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,5 @@
 import Bounded from "@/components/bounded";
-import ButtonLink from "@/components/buttonLink";
-import StarGrid from "@/components/starGrid";
+
 
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
@@ -9,6 +8,7 @@ import {
   PrismicText,
   SliceComponentProps,
 } from "@prismicio/react";
+import AnimatedContent from "./animated-content";
 
 /**
  * Props for `Hero`.
@@ -26,35 +26,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="text-center"
     >
-      <div className="relative">
-        <StarGrid />
-        {isFilled.richText(slice.primary.heading) && (
-          <h1 className="text-balanced text-center text-5xl font-medium md:text-7xl">
-            <PrismicText field={slice.primary.heading} />
-          </h1>
-        )}
-        {isFilled.richText(slice.primary.body) && (
-          <div className="text-balanced mx-auto mt-6 max-w-md text-slate-300">
-            <PrismicRichText field={slice.primary.body} />
-          </div>
-        )}
-
-        {isFilled.link(slice.primary.button_link) && (
-          <ButtonLink className="mt-8" field={slice.primary.button_link}>
-            {slice.primary.button_label}
-          </ButtonLink>
-        )}
-
-        {isFilled.image(slice.primary.image) && (
-          <div className="glass-container mt-16 w-fit">
-            <div  className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter "/>
-            <PrismicNextImage
-              field={slice.primary.image}
-              className="rounded-lg"
-            />
-          </div>
-        )}
-      </div>
+      
+      <AnimatedContent slice={slice}/>
     </Bounded>
   );
 };
